0e3446870c50a70e9afa1c1e22fb1538f5a2e9fc
SergeyKurnosov <kurnosovsergej123@mail.ru>
2026-04-20 22:22:36 +1000

    Add project files.

---
+++ RazorPages/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RazorPages.Models
+{
+	public class Student
+	{
+		public int Id { get; set; }
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public DateTime EnrollMentDate { get; set; }
+
+		//NavigationProperties
+		public ICollection<Enrollment> Enrollments { get; set; }
+	}
+}
