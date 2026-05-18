5e5ecbb09257ee3e8ce80fff44b2baeaf7376572
SergeyKurnosov <kurnosovsergej123@mail.ru>
2026-05-18 23:43:20 +1000

    concurency conflicts handle done all and razor pages done

--- RazorPages/Pages/Departments/Delete.cshtml.cs
+++ RazorPages/Pages/Departments/Delete.cshtml.cs
@@ -21,43 +21,75 @@ namespace RazorPages.Pages.Departments
 
         [BindProperty]
         public Department Department { get; set; } = default!;
+        public string ConcurencyErrorMessage { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        //public async Task<IActionResult> OnGetAsync(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var department = await _context.Departments.FirstOrDefaultAsync(m => m.DepartmentID == id);
+
+        //    if (department == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    else
+        //    {
+        //        Department = department;
+        //    }
+        //    return Page();
+        //}
+        public async Task<IActionResult> OnGetAsync(int id, bool? concurencyError)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            Department = await _context.Departments
+                .Include(d=>d.Administrator)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(d=>d.DepartmentID == id);
+            if(Department == null)return NotFound();
 
-            var department = await _context.Departments.FirstOrDefaultAsync(m => m.DepartmentID == id);
-
-            if (department == null)
-            {
-                return NotFound();
-            }
-            else
-            {
-                Department = department;
-            }
+            if (concurencyError.GetValueOrDefault())ConcurencyErrorMessage = "Удаляемая запись была изменена другим пользователем удаление было прервано если вы все таки хотите удалиь эту запись нажмите удалить еще раз";
+            
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+        //public async Task<IActionResult> OnPostAsync(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var department = await _context.Departments.FindAsync(id);
+        //    if (department != null)
+        //    {
+        //        Department = department;
+        //        _context.Departments.Remove(Department);
+        //        await _context.SaveChangesAsync();
+        //    }
+
+        //    return RedirectToPage("./Index");
+        //}
+        public async Task<IActionResult> OnPostAsync(int id)
         {
-            if (id == null)
+            try
             {
-                return NotFound();
+                if (await _context.Departments.AnyAsync(d => d.DepartmentID == id))
+                {
+                    _context.Departments.Remove(Department);
+                    await _context.SaveChangesAsync();
+                }
+                return RedirectToPage("./Index");
+            }
+            catch (DbUpdateConcurrencyException ex)
+            { 
+
+				return RedirectToPage("./Delete", new { concurencyError = true, id = id });
+
             }
 
-            var department = await _context.Departments.FindAsync(id);
-            if (department != null)
-            {
-                Department = department;
-                _context.Departments.Remove(Department);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
         }
     }
 }
